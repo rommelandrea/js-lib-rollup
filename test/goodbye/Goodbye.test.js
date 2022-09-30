4563e23a39bbf91be2b4ce27a08e4841bd6f60a6
Andrea Romanello aka rommel <rommelandrea@gmail.com>
2022-09-30 18:29:49 +0200

    fix import/export
    - fix export multiple default on src/index
    - fix extension on import

--- test/goodbye/Goodbye.test.js
+++ test/goodbye/Goodbye.test.js
@@ -1,6 +1,8 @@
 import tap from 'tap'
 import Goodbye from '../../src/goodbye/Goodbye.js'
 
+// const { Goodbye } = require('../../src')
+
 tap.test('Test sayGoodbye', async () => {
   tap.equal((new Goodbye()).sayGoodbye(), 'goodbye pippo', 'Call sayGoodbye without agruments')
   tap.equal((new Goodbye()).sayGoodbye('andrea'), 'goodbye andrea', 'Call sayGoodbye with andrea as argument')
--- test/hello/Hello.test.js
+++ test/hello/Hello.test.js
@@ -1,9 +1,7 @@
 import tap from 'tap'
-import { Hello, Goodbye } from '../src/index.js'
+import Hello from '../../src/hello/index.js'
 
 tap.test('Testing the export of libraries', async () => {
   const h = new Hello()
-  const g = new Goodbye();
   tap.equal(h.sayHello(), 'hello pippo', 'Test sayHello without param')
-  tap.equal(g.sayGoodbye(), 'goodbye pippo', 'Test sayHello without param')
 })
